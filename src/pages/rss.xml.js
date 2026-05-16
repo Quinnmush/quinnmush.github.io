9712bb05cd10f5183040d80110de3f4aae06d2c7
Quinnmush <1825717300@qq.com>
2026-05-17 01:26:58 +0800

    feat: 添加 RSS Feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,20 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { SITE } from '../config';
+
+export async function GET(context) {
+  const posts = await getCollection('posts', ({ data }) => !data.draft);
+  posts.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
+
+  return rss({
+    title: SITE.title,
+    description: SITE.description,
+    site: context.site,
+    items: posts.map(post => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
+  });
+}
